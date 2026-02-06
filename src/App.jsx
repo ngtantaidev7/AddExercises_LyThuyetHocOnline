80d3d41b2c6282c8246696096f8ca236d190147c
TaiNguyen <ngyntantai76@gmail.com>
2026-02-06 09:03:02 +0700

    Update minh chứng bài tập review React Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ApiReview from './components/ApiReview';
+import ReviewHooks from './components/ReviewHooks';
 
 function App() {
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col items-center'>
-      <ApiReview />
+      <ReviewHooks/>
     </div>
   );
 }
